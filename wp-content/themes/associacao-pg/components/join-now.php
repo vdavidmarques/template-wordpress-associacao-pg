a2f3f9e9172ea49ac3d1ddc1c4530cadf766591a
vdavidmarques <vdavidmarques@gmail.com>
2024-09-24 12:25:24 -0300

    feature: Adding join-now as component

--- wp-content/themes/associacao-pg/components/join-now.php
+++ wp-content/themes/associacao-pg/components/join-now.php
@@ -1,16 +1,32 @@
 <section class="join-now">
-    <article>   
+    <article class="container content">
         <?php 
             $title = get_field('title-join-now', $homepageId);
             $price = get_field('price-join-now', $homepageId);
             $button = get_field('button-join-now', $homepageId);
             $buttonLink = $button['url'];
             $buttonLabel = $button['title'];
-
-            echo $title;
-            echo $price;
-            echo $buttonLink;
-            echo $buttonLabel;
+            $buttonTarget = $button['target'];
         ?>
+        <div class="infos">
+            <div class="main-title">
+                <h3>
+                    <?php echo $title;?>
+                </h3>
+            </div>
+            <div class="price-tag">
+                <p class="title">A PARTIR DE</p>
+                <p>
+                    <span class="currency">R$</span>
+                    <span class="price">
+                        <?php echo $price;?>
+                    </span>
+                    <span class="mounth">/mês</span>
+                </p>
+                <a href="<?php echo $buttonLink ?> " target="<?php echo $buttonTarget ?>" class="button-join-now">
+                    <?php echo $buttonLabel;?>
+                </a>
+            </div>
+        </div>
     </article>
 </section>
