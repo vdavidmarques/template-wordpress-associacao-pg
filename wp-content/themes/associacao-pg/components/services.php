74702c51ef0c105a1e233ec15dd8597a7994d155
vdavidmarques <vdavidmarques@gmail.com>
2024-09-24 12:59:14 -0300

    feature: Adding services as component

--- wp-content/themes/associacao-pg/components/services.php
+++ wp-content/themes/associacao-pg/components/services.php
@@ -1,23 +1,27 @@
-<section class="services">
-    <?php
-    $services = get_field('services', $homepageId);
-    if (isset($services)):
-        foreach ($services as $service) :
-            $image = $service['image'];
-            $description = $service['content'];
-    ?>
-            <article class="service">
-                <?php if ($image): ?>
-                    <img src="<?php echo $image['url']; ?>" class="image" alt="<?php echo $title; ?>">
+<?php
+$services = get_field('services', $homepageId);
+if (isset($services)):
+?>
+    <section class="services">
+        <div class="container contents">
+            <div class="content">
                 <?php
-                endif;
-                if (isset($description)):
+                foreach ($services as $service) :
+                    $image = $service['image'];
+                    $description = $service['content'];
                 ?>
-                    <div class="description"><?php echo $description; ?></div>
-                <?php endif; ?>
-            </article>
-    <?php
-        endforeach;
-    endif;
-    ?>
-</section>
+                    <article class="service">
+                        <?php if ($image): ?>
+                            <div class="image">
+                                <img src="<?php echo $image['url']; ?>" class="image" alt="<?php echo $title; ?>">
+                            </div>
+                        <?php endif; ?>
+                        <?php if (isset($description)): ?>
+                            <div class="description"><?php echo $description; ?></div>
+                        <?php endif; ?>
+                    </article>
+                <?php endforeach; ?>
+            </div>
+        </div>
+    </section>
+<?php endif; ?>
